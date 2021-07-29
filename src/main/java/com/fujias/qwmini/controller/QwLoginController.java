df17abfc4fe7820172c9cc9a7f62db42100fa596
yu.yongqiang <yu.yongqiang@fujias.com>
2021-07-29 15:30:05 +0800

    登录成功

--- src/main/java/com/fujias/qwmini/controller/QwLoginController.java
+++ src/main/java/com/fujias/qwmini/controller/QwLoginController.java
@@ -6,32 +6,36 @@ import cn.hutool.json.JSON;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 
+import com.fujias.qwmini.constants.HttpStatus;
 import com.fujias.qwmini.redis.RedisService;
+import com.fujias.qwmini.results.AjaxResult;
 import com.fujias.qwmini.results.Code2SessionResult;
-import org.apache.tomcat.Jar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
-
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/qw/")
 public class QwLoginController  {
 
 
-    /*获取token地址*/
+    /* 获取token地址 */
+
     public static String TOKEN_URL = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=ID&corpsecret=SECRET";
-    /*小程序ID*/
+    /* 小程序ID */
+
     public static String CORP_ID = "ww38955021ca3074fb";
-    /*小程序密钥*/
+    /* 小程序密钥 */
+
     public static String CORPSECRET = "RkNAe_K6cMM8ojrZIm0Kz2a-sFBQNzHD_VCocihjd-g";
 
     /*临时登录凭证校验接口*/
-    public static String  CODE2SESSION_URL ="https://qyapi.weixin.qq.com/cgi-bin/miniprogram/jscode2session?access_token=ACCESS_TOKEN&js_code=CODE&grant_type=authorization_code";
-//    @Autowired
-//    private RedisTemplate redisTemplate;
 
+    public static String  CODE2SESSION_URL ="https://qyapi.weixin.qq.com/cgi-bin/miniprogram/jscode2session?access_token=ACCESS_TOKEN&js_code=CODE&grant_type=authorization_code";
+
+
+    /** redis服务 */
     @Autowired
     private RedisService redisService;
 
@@ -78,7 +82,7 @@ public class QwLoginController  {
      * @return 接口响应参数
      */
     @PostMapping("/code2Session")
-    public Code2SessionResult result(@RequestParam String code){
+    public AjaxResult AjaxResult(@RequestParam String code){
         String key = "access_token";
         String access_token;
         if (redisService.havekey(key)){
@@ -91,19 +95,30 @@ public class QwLoginController  {
             redisService.set(key,access_token);
             redisService.expire(key,expire);
         }
-        /*http请求获取到结果json串*/
+        /* http请求获取到结果json串*/
         String  jsonString =  HttpUtil.get(CODE2SESSION_URL.replaceAll("ACCESS_TOKEN",access_token).replaceAll("CODE",code));
-        /*转为JSONObject*/
+        /* 转为JSONObject*/
         JSONObject jsonObject  = JSONUtil.parseObj(jsonString);
         String sk = String.valueOf(jsonObject.get("session_key"));
-        /*转为返回参数对象*/
+        /* 转为返回参数对象*/
         Code2SessionResult result  = JSONUtil.toBean( jsonObject,Code2SessionResult.class);
 
         String session_key = result.getSession_key();
         String user_id = result.getUserid();
 
-        return result;
+        AjaxResult ajaxResult = new AjaxResult();
+        int errcode  = (int) jsonObject.get("errcode");
+        if (errcode == 0){
+            ajaxResult.put(AjaxResult.CODE_TAG, HttpStatus.SUCCESS);
+            ajaxResult.put(AjaxResult.MSG_TAG, "SUCCESS");
+            ajaxResult.put(AjaxResult.DATA_TAG,jsonObject);
+        }else{
+            ajaxResult.put(AjaxResult.CODE_TAG,jsonObject.get("errcode"));
+            ajaxResult.put(AjaxResult.MSG_TAG,jsonObject.get("errmsg"));
+        }
 
+//        return result;
+        return  ajaxResult;
 
     }
 
