e7f457baf8918e3b9977b0a943c94a1796b3efa2
yu.yongqiang <yu.yongqiang@fujias.com>
2021-07-29 09:09:46 +0800

    redis整合springboot完毕

--- src/test/java/com/fujias/qwmini/QwminiApplicationTests.java
+++ src/test/java/com/fujias/qwmini/QwminiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.fujias.qwmini;
 
+import com.alibaba.fastjson.JSONObject;
+import com.fujias.qwmini.redis.RedisService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class QwminiApplicationTests {
 
+
+    @Autowired
+    private RedisService redisService;
     @Test
     void contextLoads() {
     }
+
+    private JSONObject jsonObject = new JSONObject();
+
+    @Test
+    public void setString(){
+        redisService.set("redis setString test","success");
+    }
+
 
 }
